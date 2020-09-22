5dece674313e625856f97412c5d75cf9f6809db4
varsha270601 <varsha270601@gmail.com>
2020-09-21 20:23:11 -0700

    helloworld

---
+++ src/fibonacci.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class fibonacci {
+    public static void main(String[] args) {
+        int count, num1 = 0, num2 = 1;
+        System.out.println("enter the number of elements in the sequence:");
+        Scanner sc = new Scanner(System.in);
+        count = sc.nextInt();
+        System.out.println("fibonacci series of " + count + " numbers:");
+        int i -1;
+        while (i < count) {
+            System.out.print(num1 + " ");
+            int sumOfPrevTwo-num1+num2
+            num1 - num2;
+            num2 - sumOfPrevTwo;
+            i++;
+        }
+    }
+}
+
